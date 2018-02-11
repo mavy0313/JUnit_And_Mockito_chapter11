5a60e5893575e149f46729341dfb7a7fa1d6b8e6
mavy0313 <vypoff@gmail.com>
2018-02-11 15:30:29 +0400

    Finished refactoring Fridge Tests

--- src/test/java/exercises/FridgeRefactoredTest.java
+++ src/test/java/exercises/FridgeRefactoredTest.java
@@ -1,60 +1,66 @@
 package exercises;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.runner.RunWith;
 
 import static com.googlecode.catchexception.CatchException.catchException;
 import static com.googlecode.catchexception.CatchException.caughtException;
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
+@RunWith(JUnitParamsRunner.class)
 public class FridgeRefactoredTest {
+
+    public static final String SAUSAGE = "sausage";
+    public static final String CHEESE = "cheese";
+    public static final String HAM = "ham";
 
     private Fridge fridge = new Fridge();
 
-    @Test
-    public void testFridge() {
-        fridge.put("cheese");
-        assertEquals(true, fridge.contains("cheese"));
-        assertEquals(false, fridge.put("cheese"));
-        assertEquals(true, fridge.contains("cheese"));
-        assertEquals(false, fridge.contains("ham"));
-        fridge.put("ham");
-        assertEquals(true, fridge.contains("cheese"));
-        assertEquals(true, fridge.contains("ham"));
+    private Object[] putItems() {
+        return $($(CHEESE), $(HAM), $(SAUSAGE));
     }
 
     @Test
-    public void shouldThrowNoSuchItemException() throws NoSuchItemException {
-        catchException(fridge).take("sausage");
+    @Parameters(method = "putItems")
+    public void shouldContainPutItem(String item) {
+        fridge.put(item);
+
+        assertEquals(true, fridge.contains(item));
+    }
+
+    @Test
+    public void shouldNotContainNotPutItem() {
+        fridge.put(CHEESE);
+
+        assertEquals(false, fridge.contains(HAM));
+    }
+
+    @Test
+    public void shouldNotPutSameItemTwice() {
+        fridge.put(CHEESE);
+
+        assertEquals(false, fridge.put(CHEESE));
+    }
+
+    @Test
+    public void shouldThrowNoSuchItemExceptionWhenTakeNotPutItem() throws NoSuchItemException {
+        catchException(fridge).take(SAUSAGE);
 
         assertTrue(caughtException() instanceof NoSuchItemException );
-        assertEquals("sausage not found in the fridge", caughtException().getMessage());
+        assertEquals(SAUSAGE + " not found in the fridge", caughtException().getMessage());
     }
 
     @Test
-    public void testPutTake() throws NoSuchItemException {
-        List<String> food = new ArrayList<>();
-        food.add("yogurt");
-        food.add("milk");
-        food.add("eggs");
-        for (String item : food) {
-            fridge.put(item);
-            assertEquals(true, fridge.contains(item));
-            fridge.take(item);
-            assertEquals(false, fridge.contains(item));
-        }
-        for (String item : food) {
-            try {
-                fridge.take(item);
-                fail("there was no " + item + " in the fridge");
-            } catch (NoSuchItemException e) {
-                assertEquals(true, e.getMessage().contains(item));
-            }
-        }
+    @Parameters(method = "putItems")
+    public void shouldNotContainTakenItems(String item) throws NoSuchItemException {
+        fridge.put(item);
+        fridge.take(item);
+
+        assertEquals(false, fridge.contains(item));
     }
 
 }
